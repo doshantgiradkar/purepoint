17766a9a730edcdbdaacaac7c29c64670280a447
doShant-dev <gdoshant@gmail.com>
2025-02-22 22:49:22 +0530

    Temp Commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='bg-white'>
+      <nav className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'>
+        <h1>PurePoint</h1>
+      </nav>
+    </header>
   )
 }
 
