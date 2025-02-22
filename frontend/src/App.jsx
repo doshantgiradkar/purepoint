a1e6bb892a6782a7613852cde1f191ce8f0d760e
devmulkalwar <devmulkalwar95@gmail.com>
2025-02-22 15:49:06 +0530

    tailwind css configured

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+      <main>
+        <Outlet/>
+      </main>
+      <Footer/>
+    </div>
   )
 }
 
