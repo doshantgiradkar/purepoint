903b976423d2f2e7e2474b3d3dc74c89a6367d5c
doShant-dev <gdoshant@gmail.com>
2025-02-22 23:07:01 +0530

    Added Basic Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,15 @@
-import React from 'react'
 import Header from './components/Header'
-<<<<<<< HEAD
-=======
 import Footer from './components/Footer'
 import { Outlet } from 'react-router-dom'
->>>>>>> 64b2f296f5413f5c88b2eba2d4ec2bf8888aa3fc
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-      <Header />
-=======
       <Header/>
       <main>
         <Outlet/>
       </main>
       <Footer/>
->>>>>>> 64b2f296f5413f5c88b2eba2d4ec2bf8888aa3fc
     </div>
   )
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
     <header className='bg-white'>
       <nav className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'>
-        <h1>PurePoint</h1>
+        <h1 className="text-2xl font-bold">PurePoint</h1>
+        <Link href="/">Home</Link>
+        <Link href="/leaderboard">Leaderboard</Link>
+        <Link href="/complaints">Complaints</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/login">Login</Link>
+        <Link href="/register">Sign Up</Link>
       </nav>
     </header>
   )
