e32bec6f2aa6692572933d3d45ecb31c554fd729
devmulkalwar <devmulkalwar95@gmail.com>
2025-02-23 15:42:56 +0530

    clerk authentication implemented

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 
 const Report = () => {
   return (
-    <div>Report</div>
-  )
-}
+    <>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
 
-export default Report
+      <SignedIn>
+        <div className="text-white text-center">
+          <h1 className="text-4xl font-bold">Report a Garbage Issue</h1>
+          <p className="text-lg">Fill out the details and submit your complaint.</p>
+        </div>
+      </SignedIn>
+    </>
+  );
+};
+
+export default Report;
